ecdc97e91325d3ff8b88b431cafcad8078a26910
NamNg <nam.nguyen@gigacover.com>
2020-09-17 13:24:22 +0700

    remove harcode in info table

--- product_brand/models/sale_order.py
+++ product_brand/models/sale_order.py
@@ -5,7 +5,11 @@
 # Copyright 2018 Daniel Campos <danielcampos@avanzosc.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import api, fields, models
+from lxml import etree
+
+from odoo import api, models, _
+from odoo.exceptions import AccessError, RedirectWarning, UserError
+from odoo.tools import ustr
 
 
 class SaleOrder(models.Model):
@@ -71,3 +75,23 @@ class SaleOrder(models.Model):
             'target': 'new',
             'context': ctx,
         }
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        res = super(SaleOrder, self).fields_view_get(
+            view_id=view_id,
+            view_type=view_type,
+            toolbar=toolbar,
+            submenu=submenu
+        )
+        if view_type == 'form':
+            doc = etree.XML(res['arch'])
+            for node in doc.xpath("//button[@name='action_quotation_send']"):
+                node.set('invisible', "1")
+
+        if res.get('toolbar', False) and res.get('toolbar').get('print', False):
+            reports = res.get('toolbar').get('print')
+            for report in reports:
+                if report.get('report_file', False) and report.get('report_file') == 'sale.report_saleorder':
+                    res['toolbar']['print'].remove(report)
+        return res
