a4beb611ba7549635077111707d931402d9c2577
Nam Nguyen <ngnamuit@gmail.com>
2020-10-14 16:55:05 +0700

    add some infos for fb api

--- facebook_gr.py
+++ facebook_gr.py
@@ -8,11 +8,15 @@ print(f"graph == {graph}")
 graph.put_object("This is message for testing", "feed")
 
 
-curl -i -X GET "https://graph.facebook.com/v7.0/102384894979771_102385854979675?fields=likes.summary(true)&access_token=EAAd7ijYZCOAcBABFXJFQFs3FHZAFQO4ln9lczTTWPtoRW4nZBTfKHDkCAUw6yfGf2Es6FotaL5umLP8bqqTzbQTvDXACZB2ns50CvJmYGriQDEJ60GSxrKm9GCqD1X5ZA9cRlyCNaktB9WiMmRhS5xqszZCMKfPGl4mA9CwZCmspNdycRid5GSi1T62Md2wTnn5zUGHB5mgMwZDZD"
 
-
-curl -i -X GET "https://graph.facebook.com/1471296016504912_1960711184230057/reactions?access_token=EAAFZCeNVXZBLgBALaVFZBZCtoqoPGd2ycNgQ4RKvJTbgNogsl3k33bIcq9WGLOSjZCgHWA9TLdI1ykjvNET1XKFhFZAAmqgFZAGYOiNXOYJ5uo67h9X3jNbs0pRFl8Rkl1ZCjZBNYQkkmXGBmetWnbZCylFdqcdweNoZBZAZArumzgEPBjofqgmZAzidCjDysXEk48PCvHXVjFusdclwZDZD"
+"""
+curl -i -X GET "https://graph.facebook.com/1471296016504912_1960711184230057/reactions?access_token=EAAFZCeNVXZBLgBAGAMldZAPZANcPWFumw0Hs85lOn31hHgjdeYquxZBcIvBnyIH5KGkMZBbeJIDnL3fnG51ieoH8Sp3lkm908WefGN4QO90Vs0cbOZAHAeEYbW4na2ZCVmpsS3UcjTrL3KsAbFBNP7zQ3ydz2dJeZAs0Scbdkt9AExSTjSTiwsfNa1DTFGdZBtjJKL1XcwYj1zYgZDZD"
                                             # your_app_id  _    post_id/reactions?access_token=access_token
+                                            
+link to get token without expired date: https://business.facebook.com/
+token:                                            
+                                            
+"""
 
 
 
