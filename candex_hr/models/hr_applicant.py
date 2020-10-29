e4118aae5b65b238dd74738ecf7e89ecb44aa622
Nam Nguyen <ngnamuit@gmail.com>
2020-10-29 20:54:22 +0700

    add some field into hr candidate form

--- candex_hr/models/hr_applicant.py
+++ candex_hr/models/hr_applicant.py
@@ -17,8 +17,8 @@ class HrApplicant(models.Model):
     title_name = fields.Selection([
         ('anh', 'Anh'),
         ('chi', 'Chị'),
-        ('mr', 'Mr'),
-        ('ms', 'Ms')
+        ('mr', 'Mr.'),
+        ('ms', 'Ms.')
     ], default="mr", string='Name Title')
     first_name = fields.Char(string='First Name', translate=True)
     last_name  = fields.Char(string='Last Name', translate=True)
@@ -45,25 +45,46 @@ class HrApplicant(models.Model):
         ('widowed', 'Widowed'),
         ('others', 'Others')
     ], string='Marital Status', tracking=True)
+    job_title = fields.Selection([
+        ('cccountant', 'Accountant'),
+        ('customer_service_executive', 'Customer Service Executive'),
+        ('business_development_manager', 'Business Development Manager'),
+        ('line_technician', 'Line Technician'),
+        ('receptionlist', 'Receptionlist'),
+        ('java_developer', 'Java Developer'),
+        ('product_manager', 'Product Manager'),
+        ('purchaser', 'Purchaser'),
+        ('line_manager', 'Line Manager'),
+        ('project_manager', 'Project Manager'),
+        ('training_assistant_manager', 'Training Assistant Manager'),
+        ('recruiter', 'Recruiter'),
+        ('brand_manager', 'Brand Manager'),
+        ('marketing_executive', 'Marketing Executive'),
+        ('designer', 'Designer'),
+        ('warehouse_supervisor', 'Warehouse Supervisor'),
+        ('hr_intern', 'HR Intern'),
+        ('hr_manager', 'HR Manager'),
+        ('marketing_director', 'Marketing Director'),
+        ('head_production', 'Head of Production'),
+        ('head_sales', 'Head of Sales'),
+        ('art_director', 'Art Director'),
+        ('accountant_manager', 'Accountant Manager'),
+        ('coo', 'COO'),
+        ('others', 'Others')
+    ], string="Job Title")
     function = fields.Selection(
     [
         ('accounting', 'Accounting | Finance | Investment'),
-        ('customer', 'Customer'),
-        ('service', 'Service'),
-        ('sale', 'Sales | Business'),
-        ('development', 'Development'),
+        ('cs', 'Customer Service'),
+        ('sale_bd', 'Sales | Business Development'),
         ('hr', 'HR | Recruitment | Training'),
-        ('engineering', 'Engineering | R & D'),
-        ('administration', 'Administration | Office'),
-        ('support', 'Support'),
-        ('information', 'Information'),
-        ('technology', 'Technology'),
+        ('engineering', 'Engineering | R&D'),
+        ('administration_support', 'Administration | Office Support'),
+        ('it', 'Information Technology'),
         ('marketing', 'Marketing | Media and Communication'),
-        ('procurement', 'Procurement | Supply'),
-        ('logistic', 'Chain | Logistic'),
-        ('manufacturing', 'Production(Manufacturing)'),
-        ('consulting', 'Consulting | Project'),
-        ('management', 'Management'),
+        ('procurement_logistic', 'Procurement | Supply Chain | Logistic'),
+        ('manufacturing', 'Production (Manufacturing)'),
+        ('consulting', 'Consulting | Project Management'),
         ('others', 'Others')
     ], string='Job Function', tracking=True)
     industry = fields.Selection([
@@ -75,7 +96,7 @@ class HrApplicant(models.Model):
         ('admin_hr', 'ADMIN / HR'),
         ('accounting', 'ACCOUNTING / FINANCE'),
         ('communication_media', 'COMMUNICATION / MEDIA'),
-        ('building_construction', ' BUILDING / CONSTRUCTION'),
+        ('building_construction', 'BUILDING / CONSTRUCTION'),
         ('engineering', 'ENGINEERING'),
         ('education', 'EDUCATION / TRAINING'),
         ('healthcare', 'HEALTHCARE'),
@@ -87,13 +108,16 @@ class HrApplicant(models.Model):
         ('student', 'Student'),
         ('intern', 'Intern'),
         ('entry_level', 'Entry Level'),
-        ('senior_level', 'Senior Level(Non - Manager)'),
+        ('senior_level', 'Senior Level (Non-Manager)'),
         ('manager', 'Manager'),
         ('head', 'Head of Department'),
         ('c_level', 'C - Level')
     ], string='Seniority', tracking=True)
-    work_email = fields.Char("Email Working", tracking=True)
-    name = fields.Char("Subject / Application Name", required=False)
+    work_email  = fields.Char("Email Working", tracking=True)
+    name        = fields.Char("Subject / Application Name", required=False)
+    history_ids = fields.One2many(
+        'mail.message', 'res_id', string='Messages',
+        domain=lambda self: [('message_type', '!=', 'user_notification')], auto_join=True)
 
     @api.model
     def create(self, vals):
