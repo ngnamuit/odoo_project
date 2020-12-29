bb519073882385d136e0d1b5a2a65e0dcbe1020b
Nam Nguyen <ngnamuit@gmail.com>
2020-12-29 23:15:17 +0700

    fix hosting cache

--- module_management/controllers/main.py
+++ module_management/controllers/main.py
@@ -8,11 +8,12 @@ import werkzeug
 class ModuleManagementHome(Home):
 
     def _login_redirect(self, uid, redirect=None):
-        base_url = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
         user = request.env['res.users'].sudo().browse(uid)
+        host = request.httprequest.environ.get('HTTP_HOST', '')
+        print(f"_login_redirect self ====== {request}")
         print(f"_login_redirect USER ====== {user}")
-        print(f"_login_redirect PARAMS ====== {base_url}, {user.company_id.domain}")
-        if user and user.id not in [odoo.SUPERUSER_ID,2] and user.company_id.domain != base_url:
+        print(f"_login_redirect PARAMS ====== {host}, {user.company_id.domain}")
+        if user and user.id not in [odoo.SUPERUSER_ID,2] and host != user.company_id.domain.replace("http://", "").replace("https://", ""):
             request.session.logout()
             werkzeug.utils.redirect('/web/login', 303)
             # redirect to login page
