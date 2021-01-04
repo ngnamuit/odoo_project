d67b743de486af8c165de750b1da62279f4257b9
Nam Nguyen <ngnamuit@gmail.com>
2021-01-04 11:42:34 +0700

    verify domain, subdomain and update product master

--- module_management/controllers/main.py
+++ module_management/controllers/main.py
@@ -1,29 +1,60 @@
 # -*- coding: utf-8 -*-
 from odoo.addons.web.controllers.main import Home
+from odoo.addons.website.controllers.main import Website
 import odoo
 import odoo.modules.registry
 from odoo.http import content_disposition, dispatch_rpc, request, Response
 import werkzeug
+from odoo import http, tools
+
 
 class ModuleManagementHome(Home):
 
+    def _get_host(self, request):
+        return request.httprequest.environ.get('HTTP_HOST', '').replace("http://", "").replace("https://", "")
+
+    def _get_base_url(self, request):
+        return request.env['ir.config_parameter'].sudo().get_param('web.base.url', default='localhost:8069').\
+            replace("http://", "").replace("https://", "")
+
+    def _get_user_sub_domain(self, user):
+        return user.company_id.domain.replace("http://", "").replace("https://", "")
+
     def _login_redirect(self, uid, redirect=None):
         user = request.env['res.users'].sudo().browse(uid)
-        host = request.httprequest.environ.get('HTTP_HOST', '')
+        host = self._get_host(request)
         print(f"_login_redirect self ====== {request}")
         print(f"_login_redirect USER ====== {user}")
         print(f"_login_redirect PARAMS ====== {host}, {user.company_id.domain}")
-        if user and user.id not in [odoo.SUPERUSER_ID,2] and host != user.company_id.domain.replace("http://", "").replace("https://", ""):
+        if user and user.id not in [odoo.SUPERUSER_ID,2] and host != self._get_user_sub_domain(user):
             request.session.logout()
             werkzeug.utils.redirect('/web/login', 303)
-            # redirect to login page
-            # values = request.params.copy()
-            # try:
-            #     values['databases'] = http.db_list()
-            # except odoo.exceptions.AccessDenied:
-            #     values['databases'] = None
-            #     values['error'] = _("Wrong login/password")
-            # response = request.render('web.login', values)
-            # response.headers['X-Frame-Options'] = 'DENY'
-            # return response
         return super(ModuleManagementHome, self)._login_redirect(uid, redirect)
+
+
+    def _check_domain(self, request):
+        host = self._get_host(request)
+        base_url = self._get_base_url(request)
+        print(f"[CHECK_DOMAIN] host={host} \n base_url={base_url}")
+        if host != base_url:
+            sub_domain = host.split('.')[0]
+            ex_sub_domain = request.env['res.company'].sudo().search([('company_code', '=', sub_domain)])
+            if not ex_sub_domain:
+                print(f"[CHECK_DOMAIN] Can not found domain={sub_domain}")
+                raise request.not_found()
+        return True
+
+    @http.route('/web/login', type='http', auth="none")
+    def web_login(self, redirect=None, **kw):
+        self._check_domain(request)
+        return super(ModuleManagementHome, self).web_login(redirect, **kw)
+
+    @http.route('/', type='http', auth="none")
+    def index(self, s_action=None, db=None, **kw):
+        self._check_domain(request)
+        return super(ModuleManagementHome, self).index(s_action, db, **kw)
+
+    @http.route('/web', type='http', auth="none")
+    def web_client(self, s_action=None, **kw):
+        self._check_domain(request)
+        return super(ModuleManagementHome, self).web_client(s_action, **kw)
--- module_management/models/module_management.py
+++ module_management/models/module_management.py
@@ -21,8 +21,9 @@ class ResCertification(models.Model):
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
+    #region define model fields
     company_code = fields.Char(string='Company Code')
-    domain = fields.Char(string='Domain')
+    domain = fields.Char(string='Domain', compute="_compute_domain")
     established_year = fields.Integer(string='Established year')
     number_factories = fields.Integer(string='Number Factories')
     employees  = fields.Integer(string='Employees ')
@@ -43,17 +44,13 @@ class ResCompany(models.Model):
     )
     check_field = fields.Boolean('Check field', compute='get_user')
 
+    #endregion
+
+    #region CRUD
     def unlink(self):
         if self._uid != SUPERUSER_ID:
             raise UserError("You have no permission. Please contact admin for details!")
         return super(ResCompany, self).unlink()
-
-    @api.depends('company_code')
-    def get_user(self):
-        if SUPERUSER_ID == self._uid:
-            self.check_field = True
-        else:
-            self.check_field = False
 
     @api.model
     def create(self, values):
@@ -62,12 +59,46 @@ class ResCompany(models.Model):
             is_company = user.company_id and user.company_id.id not in [1, 2] and True or False
             if is_company:
                 raise UserError("You can not create more company. Please contact admin for details!")
+            company_code = values.get('company_code')
+            if company_code:
+                ex_company_code = self.env['res.company'].sudo().search([('company_code', '=', company_code)])
+                if ex_company_code:
+                    raise UserError("Your company code has existed already. Please choose another company code!")
         return super(ResCompany, self).create(values)
+
+    def write(self, values):
+        if values.get('attachment_ids'):
+            for rec in self:
+                company_code = values.get('company_code')
+                if company_code:
+                    ex_company_code = rec.env['res.company'].sudo().search([('company_code', '=', company_code)])
+                    if ex_company_code:
+                        raise UserError("Your company code has existed already. Please choose another company code!")
+        return super(ResCompany, self).write(values)
 
     def read(self, fields=None, load='_classic_read'):
         # NEED TO CUSTOM HERE
-        print("READ ==== ")
         return super(ResCompany, self).read(fields=fields, load=load)
+    #endregion
+
+    #region for other functions
+    @api.depends('company_code')
+    def _compute_domain(self):
+        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url', default='http://localhost:8069').replace("http://", "").replace("https://", "")
+        for res in self:
+            if res.company_code:
+                res.domain = f"{res.company_code}.{base_url}"
+            else:
+                res.domain = base_url
+
+    @api.depends('company_code')
+    def get_user(self):
+        if SUPERUSER_ID == self._uid:
+            self.check_field = True
+        else:
+            self.check_field = False
+    #endregion
+
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -76,6 +107,40 @@ class ProductTemplate(models.Model):
         user = self.env['res.users'].sudo().browse(self._uid)
         return user.company_id.id
 
-    # define fields
+    #region define fields
     company_id = fields.Many2one('res.company', 'Company', index=1,
                                  default=_default_company_id, readonly=1)
+
+    #endregion
+
+    #region CRUD
+    def write(self, values):
+        if self._uid != SUPERUSER_ID:
+            for rec in self:
+                if rec.company_id.id in [1, 2]:
+                    raise UserError("You have no permission. Please contact admin for details!")
+        return super(ProductTemplate, self).write(values)
+
+    def unlink(self):
+        if self._uid != SUPERUSER_ID:
+            raise UserError("You have no permission. Please contact admin for details!")
+        return super(ProductTemplate, self).unlink()
+    #endregion
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    #region CRUD
+    def write(self, values):
+        if self._uid != SUPERUSER_ID:
+            for rec in self:
+                if rec.company_id.id in [1, 2]:
+                    raise UserError("You have no permission. Please contact admin for details!")
+        return super(ProductProduct, self).write(values)
+
+    def unlink(self):
+        if self._uid != SUPERUSER_ID:
+            raise UserError("You have no permission. Please contact admin for details!")
+        return super(ProductProduct, self).unlink()
+    #endregion
